7f1267a30d26396bdd3e4edf51e4db112f6a2399
birockaway <bi@rockaway.cz>
2016-12-07 11:42:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -163,7 +163,7 @@ for i in range(len(scrape_dates)):
 		        #csv.register_dialect('singlequote', quotechar="'", quoting=csv.QUOTE_ALL)
 		        #csv.register_dialect('escaped', escapechar='\\', doublequote=False, quoting=csv.QUOTE_NONE)
 
-		        with open('virtualenvs/heureka/data/out/tables/'+parameters.get('Entity').get(entity).get(login).get('Shop_name')[index]+'.csv', 'ab') as output_file:
+		        with open('/data/out/tables/'+parameters.get('Entity').get(entity).get(login).get('Shop_name')[index]+'.csv', 'ab') as output_file:
                     		dict_writer = csv.DictWriter(output_file, keys, quoting=csv.QUOTE_NONNUMERIC)
                     		if (i==0):
                        			dict_writer.writeheader()
