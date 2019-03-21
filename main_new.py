e25e90833bf917752e5f4134a40a248d5dcd5a29
birockaway <bi@rockaway.cz>
2019-03-21 19:09:03 +0100

    Update main_new.py

--- main_new.py
+++ main_new.py
@@ -30,7 +30,7 @@ def validate(date_text):
 # So I can easy work with Eur or another currency in future.
 def sanitizeStrings(text):
     textSplitted = text.string.rsplit(None, 1)
-    firstResultTemp = textSplitted[0].replace(' ', '')  # if value > 999 and it result would be X XXX
+    firstResultTemp = textSplitted[0].replace('\xa0', '')  # if value > 999 and it result would be X XXX
     firstResult = firstResultTemp.replace(',', '.')
     secondResult = textSplitted[1]
     return firstResult, secondResult
@@ -119,9 +119,9 @@ for i in range(len(scrape_dates)):
                             temp = sanitizeStrings(cells[3])
                             costs = temp[0]
                             currency = temp[1]
-                            if currency == u'nbsp;Kč':
+                            if currency == u'Kč':
                                 currency = 'CZK'
-                            if currency == u'nbsp;€':
+                            if currency == u'€':
                                 currency = 'EUR'
 
                             temp = sanitizeStrings(cells[2])
